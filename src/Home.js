650fd1c44c56c232c8959356fcf1e1ff94d0d009
Andrew <atklenner@gmail.com>
2022-04-06 15:23:21 -0700

    Add home page styles

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,20 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <>
-      <Link to="/birthday">Birthday Reminder</Link>
-      <Link to="/tours">Tours</Link>
-      <Link to="/reviews">Reviews</Link>
-      <Link to="/accordion">Accordion</Link>
-      <Link to="/menu">Menu</Link>
-      <Link to="/tabs">Tabs</Link>
-      <Link to="/slider">Slider</Link>
-      <Link to="/lorem">Lorem</Link>
-      <Link to="/colors">Colors</Link>
-      <Link to="/grocery">Grocery</Link>
-    </>
+    <div className="home">
+      <h1 className="home__title">React Component Practice</h1>
+      <div className="home__links">
+        <Link to="/birthday">Birthday Reminder</Link>
+        <Link to="/tours">Tours</Link>
+        <Link to="/reviews">Reviews</Link>
+        <Link to="/accordion">Accordion</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/tabs">Tabs</Link>
+        <Link to="/slider">Slider</Link>
+        <Link to="/lorem">Lorem</Link>
+        <Link to="/colors">Colors</Link>
+        <Link to="/grocery">Grocery</Link>
+      </div>
+    </div>
   );
 }
